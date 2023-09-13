f53d426042f4199fc7b9a9a12190575ab108d515
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-13 11:40:07 +0900

    Apply style to ChatMessage component

--- src/app/components/Molecules/ChatMessage.tsx
+++ src/app/components/Molecules/ChatMessage.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Avatar } from '@mui/material';
+import React from 'react';
 
 const ChatMessage = () => {
   return (
-    <div>ChatMessage</div>
-  )
-}
+    <div className='chat-messages-message'>
+      <Avatar />
+      <div className='chat-messages-message-info'>
+        <h4>
+          TEST
+          <span className='chat-messages-message-info-timestamp'></span>
+        </h4>
+        <p>TEST</p>
+      </div>
+    </div>
+  );
+};
 
-export default ChatMessage
+export default ChatMessage;
