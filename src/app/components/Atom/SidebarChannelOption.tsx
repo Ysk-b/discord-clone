df29674b7296472e7e7263dea9744a7e2bcc9161
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-13 11:05:24 +0900

    Apply Noto Sans JP with Google fonts

--- src/app/components/Atom/SidebarChannelOption.tsx
+++ src/app/components/Atom/SidebarChannelOption.tsx
@@ -9,10 +9,10 @@ const SidebarChannelSettings = () => {
   return (
     <div className='sidebar-right-channels-settings'>
       <div className='sidebar-right-channels-settings-account'>
-        <Image src="/discordIcon.png" alt="" width={24} height={24}/>
+        <Image src='/discordIcon.png' alt='' width={32} height={32} />
         <div className='sidebar-right-channels-settings-account-name'>
           <h4>AAA</h4>
-          <span>#AAA</span>
+          <p>#AAA</p>
         </div>
       </div>
 
