65b1bf43229a47c7efcaf346553dd2a9a3239142
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-13 11:35:11 +0900

    Apply style to ChatHeader component

--- src/app/components/Molecules/ChatHeader.tsx
+++ src/app/components/Molecules/ChatHeader.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+
+import { HelpRounded, Notifications, SearchRounded, SendRounded } from '@mui/icons-material';
+import PushPinIcon from '@mui/icons-material/PushPin';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 
 const ChatHeader = () => {
   return (
-    <div>ChatHeader</div>
-  )
-}
+    <div className='chat-header'>
+      <div className='chat-header-left'>
+        <h3 className='chat-header-left-text'>
+          <span className='chat-header-left-text-hash'>#</span>
+        </h3>
+      </div>
 
-export default ChatHeader
+      <div className='chat-header-right'>
+        <Notifications />
+        <PushPinIcon />
+        <PeopleAltIcon />
+        <div className='chat-header-right-search'>
+          <input type='text' placeholder='検索' className='chat-header-right-search-input' />
+          <SearchRounded />
+        </div>
+        <SendRounded />
+        <HelpRounded />
+      </div>
+    </div>
+  );
+};
+
+export default ChatHeader;
