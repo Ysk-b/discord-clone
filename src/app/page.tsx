252bcfbe2a0f60f07a1aab01c7e041f0e7fcb345
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-13 11:24:54 +0900

    Create Chat components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '~/app/styles/App.scss';
 import Sidebar from './components/Organisms/Sidebar';
+import { Chat } from '@mui/icons-material';
 
 const App = () => {
   return (
     <div className='app'>
       <Sidebar />
+      <Chat />
     </div>
   );
 };
