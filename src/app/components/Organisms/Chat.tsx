9e98208cd1560d81a5f97dc3ad6deafc4b79fb72
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-14 10:18:33 +0900

    add; Submit state to the messages collection in Firestore

--- src/app/components/Organisms/Chat.tsx
+++ src/app/components/Organisms/Chat.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import '~/app/styles/Chat/Chat.scss';
 
 import GifIcon from '@mui/icons-material/Gif';
@@ -6,9 +8,41 @@ import { AddCircleOutline, CardGiftcardOutlined, EmojiEmotionsOutlined } from '@
 import ChatMessage from '~/app/components/Molecules/ChatMessage';
 import ChatHeader from '~/app/components/Molecules/ChatHeader';
 import { useAppSelector } from '~/app/data/hooks';
+import {
+  CollectionReference,
+  DocumentData,
+  DocumentReference,
+  addDoc,
+  collection,
+  serverTimestamp,
+} from 'firebase/firestore';
+import { db } from '~/app/data/firebase';
 
 const Chat = () => {
+  // stateで管理する入力値を送信し、Firestoreのmessages Collectionに格納したい
+  const [inputText, setInputText] = useState<string>('');
+
+  const user = useAppSelector((state) => state.user.user);
+  const channelId = useAppSelector((state) => state.channel.channelId);
   const channelName = useAppSelector((state) => state.channel.channelName);
+
+  const sendMessage = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+
+    // channelsコレクション 内の messagesコレクション 内に データ格納
+    const collectionRef: CollectionReference<DocumentData, DocumentData> = collection(
+      db,
+      'channels',
+      String(channelId),
+      'messages',
+    );
+
+    const docRef: DocumentReference<DocumentData, DocumentData> = await addDoc(collectionRef, {
+      message: inputText,
+      timestamp: serverTimestamp(),
+      user: user,
+    });
+  };
 
   return (
     <div className='chat'>
@@ -20,8 +54,17 @@ const Chat = () => {
       <div className='chat-input'>
         <AddCircleOutline className='chat-input-icon' />
         <form className='chat-input-form'>
-          <input type='text' placeholder='Send message to #TEST' />
-          <button type='submit'>Submit</button>
+          <input
+            type='text'
+            placeholder='Send message to #TEST'
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputText(e.target.value)}
+          />
+          <button
+            type='submit'
+            onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => sendMessage(e)}
+          >
+            Submit
+          </button>
         </form>
 
         <div className='chat-input-icons'>
