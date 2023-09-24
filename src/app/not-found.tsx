df030f715201d4aed234617a57638952b556b82e
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-24 15:14:49 +0900

    update: update the content of 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 const notFound = () => {
-  return <div>notFound</div>;
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <Link href='/'>Go back to home</Link>
+    </div>
+  );
 };
 
 export default notFound;
